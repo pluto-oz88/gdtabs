96a748e081c1f733d30f2174a30d26c29e32c017
Graham Dunsby <gdunsby@gmail.com>
2024-06-16 22:10:40 +1000

    25% working

--- index.php
+++ index.php
@@ -1,13 +1,45 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width,initial-scale=1.0" />
-    <title>1502 New</title>
-    <link rel="stylesheet" href="css/style.css" />
-  </head>
-  <body>
-    <script src=""></script>
-  </body>
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width,initial-scale=1.0" />
+  <title>1502 New</title>
+  <link rel="stylesheet" href="css/style.css" />
+</head>
+
+<body>
+  <div class="container">
+    <div class="tabox">
+      <label class="hb">One
+        <input type="radio" checked="checked" name="radio">
+      </label>
+      <label class="hb">Two
+        <input type="radio" name="radio">
+      </label>
+      <label class="hb">Three
+        <input type="radio" name="radio">
+      </label>
+      <label class="hb">Four
+        <input type="radio" name="radio">
+      </label>
+      <label class="hb">Five
+        <input type="radio" name="radio">
+      </label>
+      <label class="hb">Six
+        <input type="radio" name="radio">
+      </label>
+    </div>
+
+  </div>
+  <section id="sect1" class="content">
+    <h1>Tab 1</h1>
+    <h2>More crap</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus doloribus dicta ab eveniet dolorem et ut soluta, iste, optio corporis fugiat earum asperiores! Architecto sunt non officiis. Omnis eaque, assumenda aliquid sapiente a dicta dolores quibusdam placeat voluptatem libero ab magni debitis labore ipsam ea pariatur harum, rem fuga iusto? </p>\
+  </section>
+  </div>
+  <script src=""></script>
+</body>
+
 </html>
