844a30afe4e33ddc8bfe791750d6f2dac6b9c896
pluto-oz88 <gdunsby@gmail.com>
2024-06-18 00:04:39 +1000

    replaced with chatGPT version - brilliant

--- index.php
+++ index.php
@@ -1,49 +1,77 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-  <meta charset="UTF-8" />
-  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-  <meta name="viewport" content="width=device-width,initial-scale=1.0" />
-  <title>1502 New</title>
-  <link rel="stylesheet" href="css/style.css" />
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>3 Tab Webpage with Fixed Tabs</title>
+    <link rel="stylesheet" href="css/style.css">
+    
 </head>
-
 <body>
-  <div class="container">
-    <div class="tabox">
-      <label class="hb">One
-        <input type="radio" checked="checked" name="radio">
-      </label>
-      <label class="hb">Two
-        <input type="radio" name="radio">
-      </label>
-      <label class="hb">Three
-        <input type="radio" name="radio">
-      </label>
-      <label class="hb">Four
-        <input type="radio" name="radio">
-      </label>
-      <label class="hb">Five
-        <input type="radio" name="radio">
-      </label>
-      <label class="hb">Six
-        <input type="radio" name="radio">
-      </label>
+    <div class="tab-container">
+        <input type="radio" id="tab1" name="tab" checked>
+        <input type="radio" id="tab2" name="tab">
+        <input type="radio" id="tab3" name="tab">
+        <input type="radio" id="tab4" name="tab">
+        <input type="radio" id="tab5" name="tab">
+        <input type="radio" id="tab6" name="tab">
+
+
+        <div class="tabs">
+            <label for="tab1"><div>Tab 1</div></label>
+            <label for="tab2"><div>Tab 2</div></label>
+            <label for="tab3"><div>Tab 3</div></label>
+            <label for="tab4"><div>Tab 4</div></label>
+            <label for="tab5"><div>Tab 5</div></label>
+            <label for="tab6"><div>Tab 6</div></label>
+        </div>
+
+        <div class="content">
+            <div class="tab-content" id="content1">
+                <h2>Tab 1 Content</h2>
+                <p>This is the content of the first tab. You can add more text, images, or any other HTML content here.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget elementum orci. Integer vel fermentum odio. Curabitur euismod est non risus commodo, sit amet aliquet sapien euismod. Aliquam erat volutpat. Etiam fringilla vestibulum tortor, a venenatis dui scelerisque in.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget elementum orci. Integer vel fermentum odio. Curabitur euismod est non risus commodo, sit amet aliquet sapien euismod. Aliquam erat volutpat. Etiam fringilla vestibulum tortor, a venenatis dui scelerisque in.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget elementum orci. Integer vel fermentum odio. Curabitur euismod est non risus commodo, sit amet aliquet sapien euismod. Aliquam erat volutpat. Etiam fringilla vestibulum tortor, a venenatis dui scelerisque in.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget elementum orci. Integer vel fermentum odio. Curabitur euismod est non risus commodo, sit amet aliquet sapien euismod. Aliquam erat volutpat. Etiam fringilla vestibulum tortor, a venenatis dui scelerisque in.</p>
+            </div>
+            <div class="tab-content" id="content2">
+                <h2>Tab 2 Content</h2>
+                <p>This is the content of the second tab. You can add more text, images, or any other HTML content here.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget elementum orci. Integer vel fermentum odio. Curabitur euismod est non risus commodo, sit amet aliquet sapien euismod. Aliquam erat volutpat. Etiam fringilla vestibulum tortor, a venenatis dui scelerisque in.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget elementum orci. Integer vel fermentum odio. Curabitur euismod est non risus commodo, sit amet aliquet sapien euismod. Aliquam erat volutpat. Etiam fringilla vestibulum tortor, a venenatis dui scelerisque in.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget elementum orci. Integer vel fermentum odio. Curabitur euismod est non risus commodo, sit amet aliquet sapien euismod. Aliquam erat volutpat. Etiam fringilla vestibulum tortor, a venenatis dui scelerisque in.</p>
+            </div>
+            <div class="tab-content" id="content3">
+                <h2>Tab 3 Content</h2>
+                <p>This is the content of the third tab. You can add more text, images, or any other HTML content here.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget elementum orci. Integer vel fermentum odio. Curabitur euismod est non risus commodo, sit amet aliquet sapien euismod. Aliquam erat volutpat. Etiam fringilla vestibulum tortor, a venenatis dui scelerisque in.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget elementum orci. Integer vel fermentum odio. Curabitur euismod est non risus commodo, sit amet aliquet sapien euismod. Aliquam erat volutpat. Etiam fringilla vestibulum tortor, a venenatis dui scelerisque in.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget elementum orci. Integer vel fermentum odio. Curabitur euismod est non risus commodo, sit amet aliquet sapien euismod. Aliquam erat volutpat. Etiam fringilla vestibulum tortor, a venenatis dui scelerisque in.</p>
+            </div>
+            <div class="tab-content" id="content4">
+                <h2>Tab 4 Content</h2>
+                <p>This is the content of the first tab. You can add more text, images, or any other HTML content here.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget elementum orci. Integer vel fermentum odio. Curabitur euismod est non risus commodo, sit amet aliquet sapien euismod. Aliquam erat volutpat. Etiam fringilla vestibulum tortor, a venenatis dui scelerisque in.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget elementum orci. Integer vel fermentum odio. Curabitur euismod est non risus commodo, sit amet aliquet sapien euismod. Aliquam erat volutpat. Etiam fringilla vestibulum tortor, a venenatis dui scelerisque in.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget elementum orci. Integer vel fermentum odio. Curabitur euismod est non risus commodo, sit amet aliquet sapien euismod. Aliquam erat volutpat. Etiam fringilla vestibulum tortor, a venenatis dui scelerisque in.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget elementum orci. Integer vel fermentum odio. Curabitur euismod est non risus commodo, sit amet aliquet sapien euismod. Aliquam erat volutpat. Etiam fringilla vestibulum tortor, a venenatis dui scelerisque in.</p>
+            </div>
+            <div class="tab-content" id="content5">
+                <h2>Tab 5 Content</h2>
+                <p>This is the content of the second tab. You can add more text, images, or any other HTML content here.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget elementum orci. Integer vel fermentum odio. Curabitur euismod est non risus commodo, sit amet aliquet sapien euismod. Aliquam erat volutpat. Etiam fringilla vestibulum tortor, a venenatis dui scelerisque in.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget elementum orci. Integer vel fermentum odio. Curabitur euismod est non risus commodo, sit amet aliquet sapien euismod. Aliquam erat volutpat. Etiam fringilla vestibulum tortor, a venenatis dui scelerisque in.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget elementum orci. Integer vel fermentum odio. Curabitur euismod est non risus commodo, sit amet aliquet sapien euismod. Aliquam erat volutpat. Etiam fringilla vestibulum tortor, a venenatis dui scelerisque in.</p>
+            </div>
+            <div class="tab-content" id="content6">
+                <h2>Tab 6 Content</h2>
+                <p>This is the content of the third tab. You can add more text, images, or any other HTML content here.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget elementum orci. Integer vel fermentum odio. Curabitur euismod est non risus commodo, sit amet aliquet sapien euismod. Aliquam erat volutpat. Etiam fringilla vestibulum tortor, a venenatis dui scelerisque in.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget elementum orci. Integer vel fermentum odio. Curabitur euismod est non risus commodo, sit amet aliquet sapien euismod. Aliquam erat volutpat. Etiam fringilla vestibulum tortor, a venenatis dui scelerisque in.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget elementum orci. Integer vel fermentum odio. Curabitur euismod est non risus commodo, sit amet aliquet sapien euismod. Aliquam erat volutpat. Etiam fringilla vestibulum tortor, a venenatis dui scelerisque in.</p>
+            </div>
+        </div>
     </div>
-
-  </div>
-  <section id="sect1" class="content">
-    <h1>Tab 1</h1>
-    <h2>More crap</h2>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus doloribus dicta ab eveniet dolorem et ut soluta, iste, optio corporis fugiat earum asperiores! Architecto sunt non officiis. Omnis eaque, assumenda aliquid sapiente a dicta dolores quibusdam placeat voluptatem libero ab magni debitis labore ipsam ea pariatur harum, rem fuga iusto? </p>\
-  </section>
-
-  <section id="sect2" class="content">
-    <h1>Tab 2</h1>
-    <h2>More crap</h2>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus doloribus dicta ab eveniet dolorem et ut soluta, iste, optio corporis fugiat earum asperiores! Architecto sunt non officiis. Omnis eaque, assumenda aliquid sapiente a dicta dolores quibusdam placeat voluptatem libero ab magni debitis labore ipsam ea pariatur harum, rem fuga iusto? </p>\
-  </section>
-  </div>
-  <script src=""></script>
 </body>
+</html>
