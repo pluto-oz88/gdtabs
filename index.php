49cf7036dc00b05fd1ee503d727645d3bde42d99
pluto-oz88 <gdunsby@gmail.com>
2024-06-18 02:34:27 +1000

    gemini is useless

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
 </head>
 <body>
     <div class="tab-container">
+      
         <input type="radio" id="tab1" name="tab" checked>
         <input type="radio" id="tab2" name="tab">
         <input type="radio" id="tab3" name="tab">
@@ -17,30 +18,32 @@
         <input type="radio" id="tab6" name="tab">
 
 
-        <div class="tabs">
-            <label for="tab1"><div>Tab 1</div></label>
-            <label for="tab2"><div>Tab 2</div></label>
-            <label for="tab3"><div>Tab 3</div></label>
-            <label for="tab4"><div>Tab 4</div></label>
-            <label for="tab5"><div>Tab 5</div></label>
-            <label for="tab6"><div>Tab 6</div></label>
+        <div class="header">
+          <h1>TreeTopHeights.Com</h1>
+          <div class="tabs">
+              <label for="tab1"><div>Tab 1</div></label>
+              <label for="tab2"><div>Tab 2</div></label>
+              <label for="tab3"><div>Tab 3</div></label>
+              <label for="tab4"><div>Tab 4</div></label>
+              <label for="tab5"><div>Tab 5</div></label>
+              <label for="tab6"><div>Tab 6</div></label>
+          </div>
         </div>
 
         <div class="content">
             <div class="tab-content" id="content1">
                 <h2>Tab 1 Content</h2>
-                <p>This is the content of the first tab. You can add more text, images, or any other HTML content here.</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget elementum orci. Integer vel fermentum odio. Curabitur euismod est non risus commodo, sit amet aliquet sapien euismod. Aliquam erat volutpat. Etiam fringilla vestibulum tortor, a venenatis dui scelerisque in.</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget elementum orci. Integer vel fermentum odio. Curabitur euismod est non risus commodo, sit amet aliquet sapien euismod. Aliquam erat volutpat. Etiam fringilla vestibulum tortor, a venenatis dui scelerisque in.</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget elementum orci. Integer vel fermentum odio. Curabitur euismod est non risus commodo, sit amet aliquet sapien euismod. Aliquam erat volutpat. Etiam fringilla vestibulum tortor, a venenatis dui scelerisque in.</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget elementum orci. Integer vel fermentum odio. Curabitur euismod est non risus commodo, sit amet aliquet sapien euismod. Aliquam erat volutpat. Etiam fringilla vestibulum tortor, a venenatis dui scelerisque in.</p>
+                <p>MSNBC delivers breaking news, in-depth analysis of politics headlines, as well as commentary and informed perspectives. Find video clips and segments from The Rachel Maddow Show, Morning Joe, The Beat with Ari Melber, Deadline: White House, The ReidOut, All In, Last Word, 11th Hour, and Alex Wagner who brings her breadth of reporting experience to MSNBC primetime. Watch “Alex Wagner Tonight” Tuesday through Friday at 9pm Eastern. </p>
             </div>
             <div class="tab-content" id="content2">
                 <h2>Tab 2 Content</h2>
-                <p>This is the content of the second tab. You can add more text, images, or any other HTML content here.</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget elementum orci. Integer vel fermentum odio. Curabitur euismod est non risus commodo, sit amet aliquet sapien euismod. Aliquam erat volutpat. Etiam fringilla vestibulum tortor, a venenatis dui scelerisque in.</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget elementum orci. Integer vel fermentum odio. Curabitur euismod est non risus commodo, sit amet aliquet sapien euismod. Aliquam erat volutpat. Etiam fringilla vestibulum tortor, a venenatis dui scelerisque in.</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget elementum orci. Integer vel fermentum odio. Curabitur euismod est non risus commodo, sit amet aliquet sapien euismod. Aliquam erat volutpat. Etiam fringilla vestibulum tortor, a venenatis dui scelerisque in.</p>
+                <p>93,441 views  Jun 14, 2024  #GOP #Trump #Politics
+                On Thursday, Trump made his first visit to Capitol Hill since before the Jan. 6 attack as Republicans gave a hero’s welcome to their party’s presumptive 2024 nominee. The Morning Joe panel discusses.
+                » Subscribe to MSNBC:    / msnbc
+                Download our new MSNBC app for the latest breaking news and daily headlines at a glance: https://www.msnbc.com/information/dow...
+                Follow MSNBC Show Blogs 
+                MaddowBlog: https://www.msnbc.com/maddowblog
+                ReidOut Blog: https://www.msnbc.com/reidoutblog</p>
             </div>
             <div class="tab-content" id="content3">
                 <h2>Tab 3 Content</h2>
