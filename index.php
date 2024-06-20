cf9c9a6a3b3645fae1f1b6052492e1a36e430448
Graham Dunsby <gdunsby@gmail.com>
2024-06-20 11:41:35 +1000

    clean up

--- index.php
+++ index.php
@@ -1,14 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>3 Tab Webpage with Fixed Tabs</title>
-    <link rel="stylesheet" href="css/style.css">
-    
+    <link rel="stylesheet" href="css/tabs.css">
+
 </head>
+
 <body>
-<div class="tab-container">
+    <div class="tab-container">
         <input type="radio" id="tab1" name="tab" checked>
         <input type="radio" id="tab2" name="tab">
         <input type="radio" id="tab3" name="tab">
@@ -28,10 +30,10 @@
 
         <div class="content">
             <div class="tab-content" id="content1">
-            <h1>Index - Working version</h1>
-
+                <h1>Index - Fully working version</h1>
                 <h2>Tab 1 Content</h2>
-                <p>This is the content of the first tab. You can add more text, images, or any other HTML content here.</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio, facilis mollitia. Dolore labore repellendus, enim quaerat consectetur perspiciatis blanditiis eaque, repudiandae laudantium, possimus sint repellat consequuntur reiciendis eos magni? Eius, expedita! Beatae sint reiciendis ullam exercitationem, quod quidem accusantium illum eveniet facere consequuntur ut delectus veritatis temporibus odit nihil. Fuga consectetur enim quo eligendi provident maxime nam culpa laboriosam doloribus eveniet ad, ut nisi, quam iste. Velit, magnam provident non excepturi consectetur eos laboriosam doloribus molestias culpa aperiam sit vero quam at, deleniti et reprehenderit veritatis corrupti labore repellendus voluptatum possimus veniam. Enim nemo, eos cum assumenda excepturi labore aliquam recusandae tempore quaerat dicta facilis magnam obcaecati, natus, aliquid ut autem eum distinctio eaque? Alias ratione voluptates excepturi. Ut ratione porro ipsa! Natus sapiente maiores nesciunt, provident iusto temporibus, odio voluptatum eligendi sint similique sit debitis inventore corrupti quia tenetur numquam vel sunt nobis reprehenderit. Vel totam, dolores vitae laboriosam illo, illum fugiat et hic eaque repellat qui quo facilis architecto cum eveniet quibusdam corporis quod praesentium quis eius. Fuga necessitatibus nostrum totam culpa cum ut, libero, labore amet nisi perspiciatis accusantium at, fugiat excepturi quibusdam. Reiciendis sunt, tenetur cumque molestiae ex ea magni modi veritatis eius hic omnis officiis.  </p>
+                <p>This is the content of the first tab. You can add more text, images, or any other HTML content here.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio, facilis mollitia. Dolore labore repellendus, enim quaerat consectetur perspiciatis blanditiis eaque, repudiandae laudantium, possimus sint repellat consequuntur reiciendis eos magni? Eius, expedita! Beatae sint reiciendis ullam exercitationem, quod quidem accusantium illum eveniet facere consequuntur ut delectus veritatis temporibus odit nihil. Fuga consectetur enim quo eligendi provident maxime nam culpa laboriosam doloribus eveniet ad, ut nisi, quam iste. Velit, magnam provident non excepturi consectetur eos laboriosam doloribus molestias culpa aperiam sit vero quam at, deleniti et reprehenderit veritatis corrupti labore repellendus voluptatum possimus veniam. Enim nemo, eos cum assumenda excepturi labore aliquam recusandae tempore quaerat dicta facilis magnam obcaecati, natus, aliquid ut autem eum distinctio eaque? Alias ratione voluptates excepturi. Ut ratione porro ipsa! Natus sapiente maiores nesciunt, provident iusto temporibus, odio voluptatum eligendi sint similique sit debitis inventore corrupti quia tenetur numquam vel sunt nobis reprehenderit. Vel totam, dolores vitae laboriosam illo, illum fugiat et hic eaque repellat qui quo facilis architecto cum eveniet quibusdam corporis quod praesentium quis eius. Fuga necessitatibus nostrum totam culpa cum ut, libero, labore amet nisi perspiciatis accusantium at, fugiat excepturi quibusdam. Reiciendis sunt, tenetur cumque molestiae ex ea magni modi veritatis eius hic omnis officiis. </p>
             </div>
             <div class="tab-content" id="content2">
                 <h2>Tab 2 Content</h2>
@@ -43,19 +45,43 @@
             </div>
             <div class="tab-content" id="content4">
                 <h1>Index2-Best</h1>
-    
-                    <h2>Tab 4 Content</h2>
-                    <p>This is the content of the first tab. You can add more text, images, or any other HTML content here.</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio, facilis mollitia. Dolore labore repellendus, enim quaerat consectetur perspiciatis blanditiis eaque, repudiandae laudantium, possimus sint repellat consequuntur reiciendis eos magni? Eius, expedita! Beatae sint reiciendis ullam exercitationem, quod quidem accusantium illum eveniet facere consequuntur ut delectus veritatis temporibus odit nihil. Fuga consectetur enim quo eligendi provident maxime nam culpa laboriosam doloribus eveniet ad, ut nisi, quam iste. Velit, magnam provident non excepturi consectetur eos laboriosam doloribus molestias culpa aperiam sit vero quam at, deleniti et reprehenderit veritatis corrupti labore repellendus voluptatum possimus veniam. Enim nemo, eos cum assumenda excepturi labore aliquam recusandae tempore quaerat dicta facilis magnam obcaecati, natus, aliquid ut autem eum distinctio eaque? Alias ratione voluptates excepturi. Ut ratione porro ipsa! Natus sapiente maiores nesciunt, provident iusto temporibus, odio voluptatum eligendi sint similique sit debitis inventore corrupti quia tenetur numquam vel sunt nobis reprehenderit. Vel totam, dolores vitae laboriosam illo, illum fugiat et hic eaque repellat qui quo facilis architecto cum eveniet quibusdam corporis quod praesentium quis eius. Fuga necessitatibus nostrum totam culpa cum ut, libero, labore amet nisi perspiciatis accusantium at, fugiat excepturi quibusdam. Reiciendis sunt, tenetur cumque molestiae ex ea magni modi veritatis eius hic omnis officiis.  </p>
-                </div>
-                <div class="tab-content" id="content5">
-                    <h2>Tab 5 Content</h2>
-                    <p>This is the content of the second tab. You can add more text, images, or any other HTML content here.</p>
-                </div>
-                <div class="tab-content" id="content6">
-                    <h2>Tab 6 Content</h2>
-                    <p>This is the content of the third tab. You can add more text, images, or any other HTML content here.</p>
-                </div>
+
+                <h2>Tab 4 Content</h2>
+                <p>This is the content of the first tab. You can add more text, images, or any other HTML content here.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio, facilis mollitia. Dolore labore repellendus, enim quaerat consectetur perspiciatis blanditiis eaque, repudiandae laudantium, possimus sint repellat consequuntur reiciendis eos magni? Eius, expedita! Beatae sint reiciendis ullam exercitationem, quod quidem accusantium illum eveniet facere consequuntur ut delectus veritatis temporibus odit nihil. Fuga consectetur enim quo eligendi provident maxime nam culpa laboriosam doloribus eveniet ad, ut nisi, quam iste. Velit, magnam provident non excepturi consectetur eos laboriosam doloribus molestias culpa aperiam sit vero quam at, deleniti et reprehenderit veritatis corrupti labore repellendus voluptatum possimus veniam. Enim nemo, eos cum assumenda excepturi labore aliquam recusandae tempore quaerat dicta facilis magnam obcaecati, natus, aliquid ut autem eum distinctio eaque? Alias ratione voluptates excepturi. Ut ratione porro ipsa! Natus sapiente maiores nesciunt, provident iusto temporibus, odio voluptatum eligendi sint similique sit debitis inventore corrupti quia tenetur numquam vel sunt nobis reprehenderit. Vel totam, dolores vitae laboriosam illo, illum fugiat et hic eaque repellat qui quo facilis architecto cum eveniet quibusdam corporis quod praesentium quis eius. Fuga necessitatibus nostrum totam culpa cum ut, libero, labore amet nisi perspiciatis accusantium at, fugiat excepturi quibusdam. Reiciendis sunt, tenetur cumque molestiae ex ea magni modi veritatis eius hic omnis officiis. </p>
+            </div>
+            <div class="tab-content" id="content5">
+                <h2>Tab 5 Content</h2>
+                <p>This is the content of the second tab. You can add more text, images, or any other HTML content here.</p>
+            </div>
+            <div class="tab-content" id="content6">
+                <h2>Tab 6 Content</h2>
+                <p>This is the content of the third tab. You can add more text, images, or any other HTML content here.</p>
+            </div>
         </div>
     </div>
+
+    <script>
+        var header = document.querySelector(".tabs");
+        var navbarHeight = header.offsetHeight;
+        var lastScrollTop = 0;
+        console.log(navbarHeight);
+
+        window.onscroll = function() {
+            scrollHide()
+        };
+
+        function scrollHide() {
+            var st = window.pageYOffset || document.documentElement.scrollTop;
+            console.log(st);
+            if ((st > lastScrollTop) && (st > 180)) {
+                header.classList.add('hide');
+            } else {
+                header.classList.remove('hide');
+            }
+            lastScrollTop = st <= 0 ? 0 : st;
+        }
+    </script>
 </body>
+
 </html>
